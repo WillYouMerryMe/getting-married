0e4c9c6cad1ffca4120265be5b88ad0ea56793c1
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-23 22:37:16 +0900

    feat/packages(#3): DesktopButton 추가
    DesktopButton 컴포넌트를 추가했습니다

---
+++ packages/ui/src/Button/DesktopButton.tsx
@@ -0,0 +1,38 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+import { ButtonStyleType, DesktopButtonSize } from './button.type'
+import { CSSProperties, styled } from 'styled-components'
+import { flex } from '@merried/utils'
+import { getDesktopButtonSize, getButtonStyle } from './button.style'
+import { color } from '@merried/design-system'
+
+type Props = {
+	children: ReactNode
+	styleType?: ButtonStyleType
+	size?: DesktopButtonSize
+	width?: CSSProperties['width']
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const DesktopButton = ({ onClick, children, styleType = 'DEFAULT', size = 'SMALL', width, style }: Props) => {
+	return (
+		<StyledButton style={{ width, ...style }} onClick={onClick} styleType={styleType} size={size} disabled={styleType === 'DISABLED'}>
+			{children}
+		</StyledButton>
+	)
+}
+
+export default DesktopButton
+
+const StyledButton = styled.button<{
+	styleType: ButtonStyleType
+	size: DesktopButtonSize
+}>`
+	${flex({ alignItems: 'center', justifyContent: 'center' })}
+	border-radius: 8px;
+	cursor: pointer;
+	word-break: keep-all;
+
+	${(props) => props && getButtonStyle[props.styleType]};
+	${(props) => props && getDesktopButtonSize[props.size]};
+
+	${(props) => props.styleType === 'SECOND' && `color: ${color.G900};`}
+`
