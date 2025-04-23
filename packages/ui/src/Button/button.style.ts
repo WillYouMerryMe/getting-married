4016ec5a129520801e3dadb6809a896d2d3ee149
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-23 20:16:12 +0900

    feat/packages(#3): 타입에 따른 button 사이즈 추가
    사이즈에 따라 css를 적용합니다

--- packages/ui/src/Button/button.style.ts
+++ packages/ui/src/Button/button.style.ts
@@ -24,3 +24,26 @@ export const getButtonStyle = {
 		color: ${color.G0};
 	`,
 }
+
+export const getButtonSize = {
+	SMALL: css`
+		width: 166px;
+		height: 52px;
+		padding: 18px 69px;
+	`,
+	MEDIUM: css`
+		width: 185px;
+		height: 50px;
+		padding: 17px 40px;
+	`,
+	LARGE: css`
+		width: 319px;
+		height: 50px;
+		padding: 18px 120px;
+	`,
+	VERY_LARGE: css`
+		width: 351px;
+		height: 52px;
+		padding: 19px 137px;
+	`,
+}
