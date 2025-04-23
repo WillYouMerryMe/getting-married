6f9c21247827eaa9db8a1597a3882c1914d015f2
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-23 22:36:29 +0900

    feat/packages(#3): Desktopbutton 사이즈 타입 추가
    'LARGE' | 'SMALL'을 유니온 타입으로 갖는 타입을 추가했습니다

--- packages/ui/src/Button/button.type.ts
+++ packages/ui/src/Button/button.type.ts
@@ -1,3 +1,5 @@
 export type ButtonStyleType = 'DEFAULT' | 'SECOND' | 'SELECT' | 'DISABLED' | 'WARNING'
 
 export type ButtonSize = 'VERY_LARGE' | 'LARGE' | 'MEDIUM' | 'SMALL'
+
+export type DesktopButtonSize = 'LARGE' | 'SMALL'
