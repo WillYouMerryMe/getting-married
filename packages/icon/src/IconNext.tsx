a0387369285db93be007bfe85d2279cdd875ce98
junghongseop <a01082372487@gmail.com>
2025-04-28 16:24:07 +0900

    fix/packages(#8): 함수명 수정

--- packages/icon/src/IconNext.tsx
+++ packages/icon/src/IconNext.tsx
@@ -1,7 +1,7 @@
 import type { SVGProps } from 'react';
 import React from 'react';
 
-const IconPrevious = (props: SVGProps<SVGSVGElement>) => (
+const IconNext = (props: SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       d="M6.71487 18.4508L15.0482 10.1175L6.71487 1.78418"
@@ -12,4 +12,4 @@ const IconPrevious = (props: SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
-export default IconPrevious;
+export default IconNext;
