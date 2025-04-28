607d627703f7bfc7f48b4d3fbde7929fd3dda216
junghongseop <a01082372487@gmail.com>
2025-04-28 15:20:59 +0900

    feat/packages(#8): 화살표 아이콘 컴포넌트 추가

--- packages/icon/index.ts
+++ packages/icon/index.ts
@@ -1,1 +1,3 @@
 export { default as IconAdd } from './src/IconAdd';
+export { default as IconNext } from './src/IconNext';
+export { default as IconPrevious } from './src/IconPrevious';
---
+++ packages/icon/src/IconNext.tsx
@@ -0,0 +1,15 @@
+import type { SVGProps } from 'react';
+import React from 'react';
+
+const IconPrevious = (props: SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M6.71487 18.4508L15.0482 10.1175L6.71487 1.78418"
+      stroke="#B8B8B8"
+      strokeWidth="2"
+      strokeLinecap="square"
+    />
+  </svg>
+);
+
+export default IconPrevious;
