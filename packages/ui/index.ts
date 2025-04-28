997b14bd562225e4f9627ecd1625fbdbd8ca5ca8
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-28 09:45:06 +0900

    feat/packages(#6): input 컴포넌트 추가

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './src/Button/Button';
 export { default as DesktopButton } from './src/Button/DesktopButton';
 export { default as ActionButton } from './src/Button/ActionButton';
+export { default as Input } from './src/Input/Input';
---
+++ packages/ui/src/Input/Input.tsx
@@ -0,0 +1,85 @@
+import { color, font } from '@merried/design-system';
+import { css, styled } from 'styled-components';
+import type { CSSProperties, InputHTMLAttributes } from 'react';
+import type { InputSize, Platform } from './input.type';
+
+type Props = {
+  label?: string | React.ReactNode;
+  size?: InputSize;
+  platform?: Platform;
+  errorMessage?: string;
+  isError?: boolean;
+  width?: CSSProperties['width'];
+} & Omit<InputHTMLAttributes<HTMLInputElement>, 'size'>;
+
+const Input = ({
+  label,
+  platform = 'MOBILE',
+  size = 'SMALL',
+  isError = false,
+  placeholder,
+  type,
+  name,
+  value,
+  onChange,
+  readOnly,
+}: Props) => {
+  return (
+    <div>
+      {label && <Label>{label}</Label>}
+      <div style={{ position: 'relative' }}>
+        <StyledInput
+          onChange={onChange}
+          placeholder={placeholder}
+          type={type}
+          name={name}
+          value={value}
+          readOnly={readOnly}
+          $size={size}
+          $platform={platform}
+          $isError={isError}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
+
+const StyledInput = styled.input<{
+  $size: InputSize;
+  $platform: Platform;
+  $isError: boolean;
+}>`
+  ${font.Button3}
+  color: ${color.G600};
+  height: 46px;
+  padding: 12px 16px;
+  border-radius: 8px;
+  border: 1px solid ${color.G70};
+  background: ${color.G0};
+
+  &::placeholder {
+    color: ${color.G80};
+  }
+
+  &:focus {
+    border: 1px solid ${color.primary};
+  }
+
+  ${(props) =>
+    props.$isError &&
+    css`
+      border: 1px solid ${color.red};
+
+      &:focus {
+        border: 1px solid ${color.red};
+      }
+    `}
+`;
+
+const Label = styled.p`
+  ${font.P3}
+  color: ${color.G700};
+  margin-bottom: 8px;
+`;
