c57741f329296c2b041f421df56e145824f60d89
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-29 17:59:28 +0900

    feat/web(#21): 템플릿 아이템 퍼블리싱

---
+++ apps/web/src/components/template/TemplateItem/index.tsx
@@ -0,0 +1,24 @@
+import { flex } from '@merried/utils';
+import { ReactNode } from 'react';
+import { styled } from 'styled-components';
+
+interface Props {
+  filed: ReactNode;
+  title: string;
+}
+
+const TemplateItem = ({ filed, title }: Props) => {
+  return (
+    <StyledTemplateItem>
+      {filed}
+      {title}
+    </StyledTemplateItem>
+  );
+};
+
+export default TemplateItem;
+
+const StyledTemplateItem = styled.div`
+  ${flex({ flexDirection: 'column', alignItems: 'flex-start' })}
+  gap: 24px;
+`;
