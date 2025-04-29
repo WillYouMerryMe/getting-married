1ecaa9f973d702cae0056a610a0b1d784634cbcf
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-29 13:23:43 +0900

    feat/web(#15): 언어 설정 한국어

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,7 +14,7 @@ interface Props {
 
 const RootLayout = ({ children }: Props) => {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>
         <StyledComponentsRegistry>
           <QueryClientProvider>
