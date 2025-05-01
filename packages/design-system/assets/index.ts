c13b3b71bf791cd153d9ecbeadc61a273f583176
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-01 11:13:10 +0900

    feat/packages(#23): 글로벌 스타일에 폰트 적용
    font-face를 통해 폰트를 적용했습니다.

--- packages/design-system/assets/index.ts
+++ packages/design-system/assets/index.ts
@@ -1,13 +1,13 @@
 import BBB0003 from './fonts/BBB0003.woff2';
 import OwnglyphKundo from './fonts/Ownglyph_Kundo-Rg.woff2';
-import DOSGothic from './fonts/DOSGothic.woff2';
 import Diphylleia from './fonts/Diphylleia-Regular.woff2';
+import DOSGothic from './fonts/DOSGothic.woff2';
 
 const Fonts = {
   BBB0003,
   OwnglyphKundo,
-  DOSGothic,
   Diphylleia,
+  DOSGothic,
 } as const;
 
 export default Fonts;
--- packages/design-system/src/global.ts
+++ packages/design-system/src/global.ts
@@ -1,7 +1,41 @@
 import { createGlobalStyle } from 'styled-components';
+import Fonts from '../assets';
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
+
+@font-face {
+    font-family: 'BBB0003';
+    src: url(${Fonts.BBB0003}) format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Ownglyph Kundo';
+    src: url(${Fonts.OwnglyphKundo}) format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Diphylleia';
+    src: url(${Fonts.Diphylleia}) format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'DOSGothic';
+    src: url(${Fonts.DOSGothic}) format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
 
 * {
   box-sizing: border-box;
