2c83e2ac7cbdbbf8185858c8f39d64d26a8e51cc
junghongseop <a01082372487@gmail.com>
2025-05-02 22:46:49 +0900

    feat/mobile(#26): 첩정장 리스트 컴포넌트 개발

---
+++ apps/mobile/src/components/home/InvitationList/InvitationList.tsx
@@ -0,0 +1,59 @@
+import { Column, Text } from '@merried/ui';
+import InvitationItem from '../InvitationItem/InvitationItem';
+import { styled } from 'styled-components';
+import { color } from '@merried/design-system';
+import { flex } from '@merried/utils';
+
+interface InvitationItemProps {
+  title: string;
+  date: string;
+  hour: string;
+}
+
+const exampleData: InvitationItemProps[] = [
+  { title: '김예진 결혼식에 초대합니다', date: '2025.05.10', hour: '15:00' },
+  { title: '송윤서 웨딩 초대장', date: '2025.06.01', hour: '11:30' },
+  { title: '강민지 & 이준호', date: '2025.06.15', hour: '14:00' },
+  { title: '유진 & 태형 결혼식', date: '2025.07.02', hour: '16:30' },
+  { title: '지원 & 수혁 결혼식', date: '2025.07.20', hour: '13:00' },
+  { title: '소연 & 민재 초대장', date: '2025.08.05', hour: '12:00' },
+];
+
+const InvitationList = () => {
+  if (exampleData.length === 0) {
+    return (
+      <EmptyContainer>
+        <Column gap={10} alignItems="center">
+          <Text fontType="H3" color={color.G900}>
+            아직 생성된 청첩장이 없습니다
+          </Text>
+          <Text fontType="P3" color={color.G100}>
+            우리 결혼할래요 웹사이트에서 제작이 가능합니다
+          </Text>
+        </Column>
+      </EmptyContainer>
+    );
+  }
+
+  return (
+    <ListContainer>
+      {exampleData.map((item, idx) => (
+        <InvitationItem key={idx} title={item.title} date={item.date} hour={item.hour} />
+      ))}
+    </ListContainer>
+  );
+};
+
+export default InvitationList;
+
+const ListContainer = styled.div`
+  ${flex({ flexDirection: 'column' })}
+  width: 100%;
+  gap: 14px;
+`;
+
+const EmptyContainer = styled.div`
+  ${flex({ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' })}
+  height: calc(100vh - 71px - 105px - 32px);
+  width: 100%;
+`;
