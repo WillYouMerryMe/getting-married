56206fe2add09edd6e44e1f6bd3cd147b53cdb4e
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-02 19:47:02 +0900

    feat/packages(#23): JSON에서 값을 가지고 오는 함수 추가
    만약 값이 없으면 전역 값을 가지고 오는 식으로 스타일 손실을 막았습니다.

--- packages/design-system/src/template/font.ts
+++ packages/design-system/src/template/font.ts
@@ -22,3 +22,23 @@ type Templates = {
     fonts: TemplateFonts;
   };
 };
+
+const getTemplateFontStyleValue = (
+  templates: Templates,
+  templateId: string,
+  key?: string
+): FontStyle | undefined => {
+  const template = templates[templateId];
+  if (!template) return undefined;
+
+  const { global, fonts } = template;
+
+  if (!key) return global.default;
+
+  const globalStyle = global[key];
+  if (globalStyle && Object.keys(globalStyle).length > 0) {
+    return globalStyle;
+  }
+
+  return fonts[key]?.default ?? global.default;
+};
