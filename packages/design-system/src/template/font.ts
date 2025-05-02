ea28e0f4638ffdcd5065d4123ce326b172e82f20
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-02 19:38:40 +0900

    feat/packages(#23): JSON 형식에 따른 타입 추가
    폰트 프리셋 json 형식에 따른 타입을 추가했습니다.

--- packages/design-system/src/template/font.ts
+++ packages/design-system/src/template/font.ts
@@ -1,83 +1,24 @@
-import { RuleSet, css } from 'styled-components';
+type FontStyle = {
+  fontFamily?: string;
+  fontSize?: number;
+  fontWeight?: number;
+  lineHeight?: number;
+  letterSpacing?: number;
+};
 
-type FontName =
-  | 'BBB0003'
-  | 'OwnglyphKundo'
-  | 'Diphylleia'
-  | 'DOSGothic'
-  | 'KoreanCNMM'
-  | 'LeeSeoyun'
-  | 'MapoDacapo'
-  | 'OwnglyphBaekSubin'
-  | 'NelnaYesam';
+type TemplateGlobal = {
+  [section: string]: FontStyle;
+};
 
-type FontStyleMap = {
-  [templateId: string]: {
-    [fontName in FontName]: RuleSet<object>;
+type TemplateFonts = {
+  [fontName: string]: {
+    default?: FontStyle;
   };
 };
 
-const styles: FontStyleMap = {
-  template1: {
-    BBB0003: css`
-      font-family: 'BBB0003';
-      font-size: 44px;
-      font-weight: 700;
-      line-height: 110%;
-    `,
-    OwnglyphKundo: css`
-      font-family: 'Ownglyph Kundo';
-      font-size: 44px;
-      font-weight: 400;
-      line-height: 110%;
-    `,
-    Diphylleia: css`
-      font-family: 'Diphylleia';
-      font-size: 44px;
-      font-weight: 400;
-      line-height: 110%;
-    `,
-    DOSGothic: css`
-      font-family: 'DOSGothic';
-      font-size: 48px;
-      font-weight: 500;
-      line-height: 110%;
-    `,
-    KoreanCNMM: css`
-      font-family: 'KoreanCNMM';
-      font-size: 44px;
-      font-weight: 400;
-      line-height: 110%;
-    `,
-    LeeSeoyun: css`
-      font-family: 'LeeSeoyun';
-      font-size: 44px;
-      font-weight: 400;
-      line-height: 110%;
-    `,
-    MapoDacapo: css`
-      font-family: 'MapoDacapo';
-      font-size: 44px;
-      font-weight: 400;
-      line-height: 110%;
-    `,
-    OwnglyphBaekSubin: css`
-      font-family: 'Ownglyph Baek Subin';
-      font-size: 44px;
-      font-weight: 400;
-      line-height: 110%;
-    `,
-    NelnaYesam: css`
-      font-family: 'Nelna Yesam';
-      font-size: 56px;
-      font-weight: 400;
-      line-height: 110%;
-    `,
-  },
+type Templates = {
+  [templateId: string]: {
+    global: TemplateGlobal;
+    fonts: TemplateFonts;
+  };
 };
-
-const getFontStyle = (templateId: string, fontName: FontName) => {
-  return styles[templateId]?.[fontName] ?? css``;
-};
-
-export default getFontStyle;
