acaa0a6b20d35d4871a50604a3dc7d5195edd73c
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-06 20:31:43 +0900

    feat/packages(#30): 파일 업로드 기능 추가
    InsertPhoto에 파일 업로드 기능을 추가했습니다.

--- apps/web/src/components/common/InsertPhoto/InsertPhoto.tsx
+++ apps/web/src/components/common/InsertPhoto/InsertPhoto.tsx
@@ -2,18 +2,46 @@ import { color } from '@merried/design-system';
 import { flex } from '@merried/utils';
 import { styled } from 'styled-components';
 import { IconCircleAdd } from '@merried/icon';
+import { useRef, useState } from 'react';
 
 interface Props {
   size?: 'SMALL' | 'BIG';
-  value?: File[] | null;
-  onChange?: (file: File | null) => void;
   disabled?: boolean;
 }
 
-const InsertPhoto = ({ size = 'SMALL', value, onChange, disabled = false }: Props) => {
+const InsertPhoto = ({ size = 'SMALL', disabled = false }: Props) => {
+  const [image, setImage] = useState<string | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImage(reader.result as string);
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
-    <StyledInsertPhoto $size={size}>
-      <IconCircleAdd />
+    <StyledInsertPhoto onClick={() => inputRef.current?.click()} $size={size}>
+      <input
+        ref={inputRef}
+        type="file"
+        accept="image/*"
+        style={{ display: 'none' }}
+        onChange={handleUpload}
+      />
+      {image ? (
+        <img
+          src={image}
+          alt="preview"
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+      ) : (
+        <IconCircleAdd />
+      )}
     </StyledInsertPhoto>
   );
 };
@@ -29,4 +57,6 @@ const StyledInsertPhoto = styled.div<{
   background: ${color.G0};
   width: ${({ $size }) => ($size === 'SMALL' ? '140px' : '384px')};
   height: 140px;
+
+  cursor: pointer;
 `;
