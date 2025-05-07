21a07a3c2aa541af172a6835a714305a4382844c
junghongseop <a01082372487@gmail.com>
2025-05-08 08:05:44 +0900

    feat/mobile(#33): 버튼 그룹 컴포넌트 개발

---
+++ apps/mobile/src/components/common/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,50 @@
+import { color } from '@merried/design-system';
+import { Button, Row, Text } from '@merried/ui';
+import { flex } from '@merried/utils';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+interface ButtonItem {
+  label: string;
+  onClick: () => void;
+}
+
+interface ButtonGroupProps {
+  title: string;
+  buttons: ButtonItem[];
+}
+
+const ButtonGroup = ({ title, buttons }: ButtonGroupProps) => {
+  const [selectedButton, setSelectedButton] = useState<number>(0);
+
+  return (
+    <StyledButtonGroup>
+      <Text fontType="P2" color={color.G900}>
+        {title}
+      </Text>
+      <Row alignItems="center" gap={19} width="100%">
+        {buttons.map((button, index) => (
+          <Button
+            key={index}
+            styleType={selectedButton === index ? 'DEFAULT' : 'SELECT'}
+            width="100%"
+            onClick={() => {
+                setSelectedButton(index);
+              button.onClick();
+            }}
+          >
+            {button.label}
+          </Button>
+        ))}
+      </Row>
+    </StyledButtonGroup>
+  );
+};
+
+export default ButtonGroup;
+
+const StyledButtonGroup = styled.div`
+  ${flex({ flexDirection: 'column', alignItems: 'flex-start' })}
+  width: 100%;
+  gap: 10px;
+`;
