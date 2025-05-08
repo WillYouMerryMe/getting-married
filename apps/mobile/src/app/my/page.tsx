80de61c38f684ecf48345c95ef57e5b6d99be5b2
junghongseop <a01082372487@gmail.com>
2025-05-08 15:56:45 +0900

    feat/mobile(#40): 마이페이지 퍼블리싱

--- apps/mobile/src/app/my/page.tsx
+++ apps/mobile/src/app/my/page.tsx
@@ -1,14 +1,34 @@
 'use client';
 
+import MyInfoList from '@/components/my/MyInfoList/MyInfoList';
 import AppLayout from '@/layouts/AppLayout';
 import { color } from '@merried/design-system';
+import { Text } from '@merried/ui';
+import { flex } from '@merried/utils';
+import styled from 'styled-components';
 
 const My = () => {
   return (
-    <AppLayout footer backgroundColor={color.G20}>
-      마이페이지
+    <AppLayout footer backgroundColor={color.G0}>
+      <StyledMy>
+        <Text fontType="H2" color={color.G900}>
+          마이페이지
+        </Text>
+        <MyInfoList />
+      </StyledMy>
     </AppLayout>
   );
 };
 
 export default My;
+
+const StyledMy = styled.div`
+  ${flex({
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  })}
+  width: 100%;
+  gap: 49px;
+  padding: 67px 18px 115px;
+`;
