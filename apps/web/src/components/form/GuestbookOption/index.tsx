e984b2311abd67aab9b6647bed4007b0af5b66dd
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-10 19:26:19 +0900

    feat/web(#45): 방명록 옵션 퍼블리싱

---
+++ apps/web/src/components/form/GuestbookOption/index.tsx
@@ -0,0 +1,55 @@
+import { color } from '@merried/design-system';
+import { IconDragHandle } from '@merried/icon';
+import { Column, Input, Row, Text, ToggleButton } from '@merried/ui';
+import { flex } from '@merried/utils';
+import { styled } from 'styled-components';
+
+const GuestbookOption = () => {
+  return (
+    <StyledGuestbookOption>
+      <Column gap={28}>
+        <Row gap={8}>
+          <ToggleButton isOpen={false} />
+          <Text fontType="H3" color={color.G900}>
+            방명록
+          </Text>
+        </Row>
+        <Column gap={8}>
+          <Text fontType="P3" color={color.G900}>
+            제목
+          </Text>
+          <Input width={384} platform="DESKTOP" placeholder="제목을 입력해주세요" />
+        </Column>
+        <Column gap={8}>
+          <Text fontType="P3" color={color.G900}>
+            마스터 비밀번호<RequiredMark>*</RequiredMark>
+          </Text>
+          <Input
+            width={384}
+            platform="DESKTOP"
+            placeholder="방명록을 열 비밀번호를 입력해주세요"
+          />
+        </Column>
+      </Column>
+      <IconDragHandle />
+    </StyledGuestbookOption>
+  );
+};
+
+export default GuestbookOption;
+
+const StyledGuestbookOption = styled.div`
+  ${flex({ justifyContent: 'space-between', alignItems: 'flex-start' })}
+  padding: 28px 20px;
+  border-radius: 8px;
+  background: ${color.G0};
+`;
+
+const RequiredMark = styled.span`
+  color: ${color.red};
+  font-family: 'Pretendard Variable';
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 100%;
+`;
