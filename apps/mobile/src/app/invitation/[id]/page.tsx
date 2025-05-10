d0aac3bd8f7ec6ef59b89fd0a0057bf92d7fe6aa
junghongseop <a01082372487@gmail.com>
2025-05-10 21:53:55 +0900

    feat/mobile(#47): 청첩장 페이지 추가

---
+++ apps/mobile/src/app/invitation/[id]/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import AppLayout from '@/layouts/AppLayout';
+import { color } from '@merried/design-system';
+
+const InvitationDetail = () => {
+  return <AppLayout backgroundColor={color.G0}>청첩장 미리보기 페이지</AppLayout>;
+};
+
+export default InvitationDetail;
