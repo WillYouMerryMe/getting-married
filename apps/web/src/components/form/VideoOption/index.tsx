92275d83f15e32331712775a46e16a88efdc6bf5
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-10 19:10:56 +0900

    feat/web(#45): 영상 옵션 퍼블리싱

---
+++ apps/web/src/components/form/VideoOption/index.tsx
@@ -0,0 +1,51 @@
+import { color } from '@merried/design-system';
+import { IconDragHandle } from '@merried/icon';
+import { Column, Input, Row, Text, ToggleButton } from '@merried/ui';
+import { flex } from '@merried/utils';
+import { styled } from 'styled-components';
+
+const VideoOption = () => {
+  return (
+    <StyledVideoOption>
+      <Column gap={28}>
+        <Row gap={8}>
+          <ToggleButton isOpen={false} />
+          <Text fontType="H3" color={color.G900}>
+            영상
+          </Text>
+        </Row>
+        <Column gap={8}>
+          <Text fontType="P3" color={color.G900}>
+            영상 제목
+          </Text>
+          <Input width={384} platform="DESKTOP" placeholder="제목을 입력해주세요" />
+        </Column>
+        <Column gap={8}>
+          <Text fontType="P3" color={color.G900}>
+            유튜브 URL<RequiredMark>*</RequiredMark>
+          </Text>
+          <Input width={384} platform="DESKTOP" placeholder="URL을 입력해주세요" />
+        </Column>
+      </Column>
+      <IconDragHandle />
+    </StyledVideoOption>
+  );
+};
+
+export default VideoOption;
+
+const StyledVideoOption = styled.div`
+  ${flex({ justifyContent: 'space-between', alignItems: 'flex-start' })}
+  padding: 28px 20px;
+  border-radius: 8px;
+  background: ${color.G0};
+`;
+
+const RequiredMark = styled.span`
+  color: ${color.red};
+  font-family: 'Pretendard Variable';
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 100%;
+`;
