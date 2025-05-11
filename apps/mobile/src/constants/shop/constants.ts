d049dd436e9ef67d3b0ea4734256bca3d9270546
junghongseop <a01082372487@gmail.com>
2025-05-12 07:41:53 +0900

    add/mobile(#58): 답례품 이름 상세화

--- apps/mobile/src/constants/shop/constants.ts
+++ apps/mobile/src/constants/shop/constants.ts
@@ -1,4 +1,4 @@
-import { Meal, Paid, Status } from '@/types/shop/client';
+import { Gift, Meal, Paid, Status } from '@/types/shop/client';
 
 export const STATUS_OPTIONS = [
   { value: 'ATTEND', label: '참석' },
@@ -28,4 +28,11 @@ export const MEAL_OPTIONS = [
 export const MEAL: Record<Meal, string> = {
   MEAL: '식권 O',
   NOMEAL: '식권 X',
+};
+
+export const GIFT: Record<Gift, string> = {
+  신세계상품권: '신세계상품권 1만원권',
+  롯데백화점상품권: '롯데백화점 상품권 1만원권',
+  현대백화점상품권: '현대백화점 상품권 1만원권',
+  올리브영: '올리브영 모바일 금액권 1만원권',
 };
