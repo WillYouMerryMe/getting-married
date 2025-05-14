098b4bbea85e6d9e4edea57b7a27d956828b74ba
junghongseop <a01082372487@gmail.com>
2025-05-14 17:04:51 +0900

    style/packages(#68): 형식 일치

--- packages/design-system/src/global.ts
+++ packages/design-system/src/global.ts
@@ -98,6 +98,7 @@ const GlobalStyle = createGlobalStyle`
   src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_yuniverse@1.0/YUniverse-B.woff2') format('woff2');
   font-weight: normal;
   font-style: normal;
+  font-display: swap;
 }
 
 * {
