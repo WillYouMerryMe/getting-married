5475e3c7953b7225045b92e8b1f6a1d11be688cd
junghongseop <a01082372487@gmail.com>
2025-05-16 08:53:52 +0900

    feat/mobile(#79): 참석여부 바텀 시트 퍼블리싱

---
+++ apps/mobile/src/components/invitation/AttendBottomSheet/AttendBottomSheet.tsx
@@ -0,0 +1,77 @@
+import BottomSheet from '@/components/common/BottomSheet/BottomSheet';
+import ButtonGroup from '@/components/common/ButtonGroup/ButtonGroup';
+import DesktopButtonGroup from '@/components/common/ButtonGroup/DesktopButtonGroup';
+import { color } from '@merried/design-system';
+import { Button, Column, Input, Text } from '@merried/ui';
+import { flex } from '@merried/utils';
+import { styled } from 'styled-components';
+
+interface AttendBottomSheetProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const AttendBottomSheet = ({ isOpen, onClose }: AttendBottomSheetProps) => {
+  return (
+    <BottomSheet isOpen={isOpen} onClose={onClose}>
+      <StyledAttendBottomSheet>
+        <Column gap={40}>
+          <Column gap={24}>
+            <Column>
+              <Text fontType="H2" color={color.G900}>
+                결혼식 참석 의사
+              </Text>
+              <Text fontType="P2" color={color.G80}>
+                솔직한 답은 신랑•신부에게 도움이 됩니다
+              </Text>
+            </Column>
+            <ButtonGroup
+              buttons={[
+                { label: '신랑측', onClick: () => {} },
+                { label: '신부측', onClick: () => {} },
+              ]}
+              pointColor={color.pointYellow}
+            />
+            <Column gap={18}>
+              <Input
+                label="이름"
+                placeholder="이름을 입력해주세요"
+                size="LARGE"
+                width="100%"
+                onChange={() => {}}
+              />
+              <Input label="참석인원" size="LARGE" onChange={() => {}} />
+              <DesktopButtonGroup
+                title="식사 여부"
+                buttons={[
+                  { label: '예정', onClick: () => {} },
+                  { label: '안함', onClick: () => {} },
+                  { label: '미정', onClick: () => {} },
+                ]}
+                pointColor={color.pointYellow}
+              />
+            </Column>
+          </Column>
+          <Button
+            size="VERY_LARGE"
+            onClick={() => {}}
+            pointColor={color.pointYellow}
+            width="100%"
+          >
+            <Text fontType="Button3" color={color.G900}>
+              참석 의사 전달하기
+            </Text>
+          </Button>
+        </Column>
+      </StyledAttendBottomSheet>
+    </BottomSheet>
+  );
+};
+
+export default AttendBottomSheet;
+
+const StyledAttendBottomSheet = styled.div`
+  ${flex({ flexDirection: 'column', alignItems: 'stretch' })}
+  width: 100%;
+  margin-top: 40px;
+`;
