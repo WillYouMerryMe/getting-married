de780c0be2d35016e2e8c09c71cbb54a5211cbde
junghongseop <a01082372487@gmail.com>
2025-05-15 14:58:36 +0900

    feat/mobile(#77): 참석 플로팅 버튼 개발

---
+++ apps/mobile/src/components/invitation/AttendFloatButton/AttendFloatButton.tsx
@@ -0,0 +1,41 @@
+import { color } from '@merried/design-system';
+import { Text } from '@merried/ui';
+import { flex } from '@merried/utils';
+import styled from 'styled-components';
+
+const AttendFloatButton = () => {
+  return (
+    <StyledAttendFloatButton onClick={() => {}}>
+      <ActionButton>
+        <Text fontType="Button3" color={color.G900}>
+          참석 의사 전달하기
+        </Text>
+      </ActionButton>
+    </StyledAttendFloatButton>
+  );
+};
+
+export default AttendFloatButton;
+
+const StyledAttendFloatButton = styled.div`
+  position: fixed;
+  bottom: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 42px;
+  border-radius: 28px;
+`;
+
+const ActionButton = styled.div`
+  ${flex({ alignItems: 'center', justifyContent: 'center' })}
+  height: 42px;
+  box-shadow:
+    -2px 0px 6px 0px rgba(200, 200, 200, 0.25),
+    3px 3px 6px 0px rgba(200, 200, 200, 0.25);
+  background-color: ${color.pointYellow};
+  padding: 14px 24px;
+  border-radius: 999px;
+`;
