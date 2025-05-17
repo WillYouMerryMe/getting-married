ea038c0c80f70e0379c9bed7680195960bf05542
junghongseop <a01082372487@gmail.com>
2025-05-17 15:55:11 +0900

    fix/mobile(#75): 페이지에 적용

--- apps/mobile/src/app/invitation/[id]/page.tsx
+++ apps/mobile/src/app/invitation/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import AttendFloatButton from '@/components/invitation/AttendFloatButton/AttendFloatButton';
 import CoupleInfo from '@/components/invitation/CoupleInfo/CoupleInfo';
+import Direction from '@/components/invitation/Direction/Direction';
 import Greeting from '@/components/invitation/Greeting/Greeting';
 import StartBackground from '@/components/invitation/StartBackground/StartBackground';
 import WeddingAlbum from '@/components/invitation/WeddingAlbum/WeddingAlbum';
@@ -50,6 +51,17 @@ const InvitationDetail = () => {
             vedioURL={
               'https://www.youtube.com/embed/-LtlYB6Iws8?rel=0&autoplay=1&mute=1&loop=1&playlist=-LtlYB6Iws8'
             }
+          />
+          <Direction
+            address="경상남도 창원시 성산구 원이대로 332"
+            detailAddress="그랜드 머큐어 앰배서더 창원"
+            methods={{
+              버스: '센트럴시티 앞 또는 고속터미널 앞(호남선) 하차 → 에스컬레이터 이용 → 신세계백화점 2층 불가리 매장 옆 아케이드 → 호텔 로비',
+              지하철:
+                '3, 7, 9호선 고속터미널역 하차\n- 3, 9호선 고속터미널역 7번 출구 → 나가기 전 우측 센트럴시티 연결 통로 → 에스컬레이터 이용 → 신세계백화점 2층 불가리 매장 옆 아케이드 → 호텔 로비\n\n- 7호선 고속터미널역 3번 출구 → 좌측 호텔 입구',
+              자가용:
+                '신세계 센트럴 주차장을 이용하시고, 직원에게 주차 인증을 받으시기 바랍니다. 자세한 사항은 유선번호\n(02-6282-6262)로 문의하시기 바랍니다.',
+            }}
           />
           <Text fontType="P4" color={color.G80}>
             COPYRIGHT Kangwon Park. All rights reserved.
