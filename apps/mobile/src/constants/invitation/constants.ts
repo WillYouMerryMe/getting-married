08ca93d3c5d6030db76b462e9d186df0f739ece0
junghongseop <a01082372487@gmail.com>
2025-05-17 15:52:35 +0900

    feat/mobile(#75): 교통수단 상수로 추가

--- apps/mobile/src/constants/invitation/constants.ts
+++ apps/mobile/src/constants/invitation/constants.ts
@@ -2,3 +2,7 @@ export const GENDER_MAP = {
   SON: '아들',
   DAUGHTER: '딸',
 } as const;
+
+export const TRANSPORT_TYPES = ['버스', '지하철', '자가용'] as const;
+
+export type TransportType = (typeof TRANSPORT_TYPES)[number];
