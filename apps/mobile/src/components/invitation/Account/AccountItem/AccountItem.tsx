abdf8b9d0c0c4618f6c7bc3c93fe91d580ff9b94
junghongseop <a01082372487@gmail.com>
2025-05-18 00:53:47 +0900

    feat/mobile(#83): 계좌번호 리스트 아이템 컴포넌트 개발

---
+++ apps/mobile/src/components/invitation/Account/AccountItem/AccountItem.tsx
@@ -0,0 +1,80 @@
+import { Account } from '@/types/invitation/client';
+import { color } from '@merried/design-system';
+import { ActionButton, Column, Text } from '@merried/ui';
+import { flex } from '@merried/utils';
+import styled from 'styled-components';
+
+interface AccountItemProps {
+  type: string;
+  accounts: Account[];
+}
+
+const AccountItem = ({ type, accounts }: AccountItemProps) => (
+  <StyledAccountItem>
+    <Column gap={10} width="100%">
+      <Text fontType="H4" color={color.G900}>
+        {type}측 계좌번호
+      </Text>
+      <Line />
+    </Column>
+    <StyledAccountContent>
+      <BlurArea>
+        {accounts.map(({ bank, account, name }, i) => (
+          <Column key={i} gap={8} width="100%">
+            <Text fontType="P2" color={color.G900}>
+              {bank} {account}
+            </Text>
+            <Text fontType="P2" color={color.G900}>
+              {name}
+            </Text>
+          </Column>
+        ))}
+      </BlurArea>
+      <ButtonWrapper>
+        <ActionButton background={color.pointYellow} onClick={() => {}}>
+          <Text fontType="Button3" color={color.G900}>
+            계좌번호 보기
+          </Text>
+        </ActionButton>
+      </ButtonWrapper>
+    </StyledAccountContent>
+  </StyledAccountItem>
+);
+
+export default AccountItem;
+
+const StyledAccountItem = styled.div`
+  ${flex({ flexDirection: 'column', alignItems: 'flex-start' })}
+  width: 100%;
+`;
+
+const Line = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: ${color.G50};
+`;
+
+const StyledAccountContent = styled.div`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  padding-top: 10px;
+`;
+
+const BlurArea = styled.div`
+  ${flex({ flexDirection: 'column' })}
+  gap: 14px;
+  filter: blur(10px);
+  position: relative;
+  z-index: 1;
+`;
+
+const ButtonWrapper = styled.div`
+  ${flex({ alignItems: 'center', justifyContent: 'center' })}
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+`;
