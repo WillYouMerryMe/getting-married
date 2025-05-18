a149face3da4cdf056592323ff977bf6f73644ac
junghongseop <a01082372487@gmail.com>
2025-05-19 08:26:06 +0900

    fix/mobile(#85): 방명록 단계 상태 관리 추가

---
+++ apps/mobile/src/stores/invitation/guestbookStep.ts
@@ -0,0 +1,11 @@
+import { GuestBook } from '@/types/invitation/client';
+import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+
+const guestbookStepAtomState = atom<GuestBook>({
+  key: 'guestbook-step',
+  default: '비밀번호 입력',
+});
+
+export const useGuestBookStepStore = () => useRecoilState(guestbookStepAtomState);
+export const useSetGuestBookStepStore = () => useSetRecoilState(guestbookStepAtomState);
+export const useGuestBookStepValueStore = () => useRecoilValue(guestbookStepAtomState);
