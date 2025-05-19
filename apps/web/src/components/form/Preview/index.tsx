cd9098eff048743fb472696c7bf93ba6217b661d
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-19 22:41:03 +0900

    feat/web(#63): 자동 포커스 핸들링 부문 퍼블리싱

--- apps/web/src/components/form/Preview/index.tsx
+++ apps/web/src/components/form/Preview/index.tsx
@@ -1,3 +1,5 @@
+import { color } from '@merried/design-system';
+import { Column, Row, Text, ToggleButton } from '@merried/ui';
 import { flex } from '@merried/utils';
 import { styled } from 'styled-components';
 
@@ -9,6 +11,17 @@ const Preview = ({ id }: Props) => {
   return (
     <StyledPreview>
       <ImageLayer />
+      <Row gap={8} alignItems="center">
+        <ToggleButton isOpen={true} />
+        <Column gap={4}>
+          <Text fontType="H4" color={color.G900}>
+            자동 포커스
+          </Text>
+          <Text fontType="P3" color={color.G80}>
+            수정하는 부분을 따라 포커스가 바뀝니다.
+          </Text>
+        </Column>
+      </Row>
     </StyledPreview>
   );
 };
