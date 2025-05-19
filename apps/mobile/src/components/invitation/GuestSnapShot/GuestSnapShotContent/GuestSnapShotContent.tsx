d2d17b709614126c5171152cf09096594a27b56f
junghongseop <a01082372487@gmail.com>
2025-05-19 10:41:03 +0900

    fix/mobile(#92): 에러 뜨도록 수정

--- apps/mobile/src/components/invitation/GuestSnapShot/GuestSnapShotContent/GuestSnapShotContent.tsx
+++ apps/mobile/src/components/invitation/GuestSnapShot/GuestSnapShotContent/GuestSnapShotContent.tsx
@@ -4,6 +4,8 @@ import { flex } from '@merried/utils';
 import { useState } from 'react';
 import styled from 'styled-components';
 
+const MAX_FILES = 19;
+
 const GuestSnapShotContent = () => {
   const [image, setImage] = useState<string[] | null>(null);
 
@@ -12,12 +14,12 @@ const GuestSnapShotContent = () => {
   return (
     <StyledGuestSnapShotContent>
       <Column gap={14} alignItems="stretch" width="100%">
-        <InsertPhoto size="BIG" value={image} onChange={setImage} maxFiles={19} />
+        <InsertPhoto size="BIG" value={image} onChange={setImage} maxFiles={MAX_FILES} />
         <Column gap={4}>
           <Text fontType="P3" color={color.G80}>
             · 1장당 100MB까지 업로드 가능합니다.
           </Text>
-          <Text fontType="P3" color={color.G80}>
+          <Text fontType="P3" color={count >= MAX_FILES ? color.red : color.G80}>
             · 최대 20장 업로드 가능합니다.
           </Text>
         </Column>
