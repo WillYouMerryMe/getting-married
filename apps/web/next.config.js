2438bd98f2513cb91de5c97ea28b4224274988bf
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-20 19:40:31 +0900

    fix/web(#63): build 안되는 에러
    next 버전 다운그레이드로 인한 config 관련 변경

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,4 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
