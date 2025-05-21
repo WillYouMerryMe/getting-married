b9b369a2474bbe9604133b91930b77f926f58148
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-21 23:18:53 +0900

    feat/web(#63): 갤러리 옵션 전역변수 추가

---
+++ apps/web/src/store/form/galleryImage.ts
@@ -0,0 +1,20 @@
+import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+
+interface GalleryImageState {
+  title: string;
+  imageList: string[] | null;
+}
+
+const defaultCeremonyInfo: GalleryImageState = {
+  title: '',
+  imageList: [],
+};
+
+const galleryImageAtomState = atom<GalleryImageState>({
+  key: 'gallery-image-state',
+  default: defaultCeremonyInfo,
+});
+
+export const useGalleryImageStore = () => useRecoilState(galleryImageAtomState);
+export const useSetGalleryImageStore = () => useSetRecoilState(galleryImageAtomState);
+export const useGalleryImageValueStore = () => useRecoilValue(galleryImageAtomState);
