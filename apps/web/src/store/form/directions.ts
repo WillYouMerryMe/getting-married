7de376e4face339ab5363f980aec7408fb59d5fe
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-22 11:58:36 +0900

    feat/web(#63): 오시는 길 옵션 전역상태 추가

---
+++ apps/web/src/store/form/directions.ts
@@ -0,0 +1,32 @@
+import { atom, useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
+
+type TransportType = 'subway' | 'bus' | 'car';
+
+interface DirectionsState {
+  address: string;
+  methods: Partial<Record<TransportType, string>>;
+  show: Record<TransportType, boolean>;
+}
+
+const defaultDirections: DirectionsState = {
+  address: '',
+  methods: {
+    subway: '',
+    bus: '',
+    car: '',
+  },
+  show: {
+    subway: false,
+    bus: false,
+    car: false,
+  },
+};
+
+const DirectionsAtomState = atom<DirectionsState>({
+  key: 'directions-state',
+  default: defaultDirections,
+});
+
+export const useDirectionsStore = () => useRecoilState(DirectionsAtomState);
+export const useSetDirectionsStore = () => useSetRecoilState(DirectionsAtomState);
+export const useDirectionsValueStore = () => useRecoilValue(DirectionsAtomState);
