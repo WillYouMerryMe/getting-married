8b555f9aa9adfa8825509a8290fcc658286b6605
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-22 20:07:12 +0900

    feat/web(#63): 계좌 번호 옵션 전역변수로 상태 저장

--- apps/web/src/components/form/options/AccountInfoOption/index.tsx
+++ apps/web/src/components/form/options/AccountInfoOption/index.tsx
@@ -1,30 +1,19 @@
+import { useAccountInfoStore } from '@/store/form/account';
 import { color } from '@merried/design-system';
 import { CheckBox, Column, Input, Row, Text, ToggleButton } from '@merried/ui';
-import { useReducer } from 'react';
-import type { AccountState, AccountAction, AccountField } from '@/types/form/client';
 
-const reducer = (state: AccountState, action: AccountAction): AccountState => {
-  switch (action.type) {
-    case 'TOGGLE':
-      return {
-        ...state,
-        [action.payload]: !state[action.payload],
-      };
-    default:
-      return state;
-  }
-};
+type AccountField =
+  | 'groom'
+  | 'groomFather'
+  | 'groomMother'
+  | 'bride'
+  | 'brideFather'
+  | 'brideMother';
 
-const initialState: AccountState = {
-  groom: false,
-  bride: false,
-  groomFather: false,
-  groomMother: false,
-  brideFather: false,
-  brideMother: false,
-};
-
-const accountOptions: { label: string; key: AccountField }[] = [
+const accountOptions: {
+  label: string;
+  key: AccountField;
+}[] = [
   { label: '신랑', key: 'groom' },
   { label: '신부', key: 'bride' },
   { label: '신랑 아버지', key: 'groomFather' },
@@ -34,7 +23,37 @@ const accountOptions: { label: string; key: AccountField }[] = [
 ];
 
 const AccountInfoOption = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [accountInfo, setAccountInfo] = useAccountInfoStore();
+
+  const handleTitleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setAccountInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handlePersonInputChange =
+    (personKey: AccountField) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setAccountInfo((prev) => ({
+        ...prev,
+        [personKey]: {
+          ...prev[personKey],
+          [name]: value,
+        },
+      }));
+    };
+
+  const handleShowChange = (personKey: AccountField) => (value: boolean) => {
+    setAccountInfo((prev) => ({
+      ...prev,
+      [personKey]: {
+        ...prev[personKey],
+        isVisible: value,
+      },
+    }));
+  };
 
   return (
     <Column gap={28}>
@@ -48,34 +67,69 @@ const AccountInfoOption = () => {
         <Text fontType="P3" color={color.G900}>
           제목
         </Text>
-        <Input width={384} platform="DESKTOP" placeholder="제목을 입력해주세요" />
+        <Input
+          name="title"
+          width={384}
+          platform="DESKTOP"
+          placeholder="제목을 입력해주세요"
+          value={accountInfo.title}
+          onChange={handleTitleMessageChange}
+        />
       </Column>
       <Column gap={8}>
         <Text fontType="P3" color={color.G900}>
           내용
         </Text>
-        <Input width={384} platform="DESKTOP" placeholder="내용을 입력해주세요" />
+        <Input
+          name="message"
+          width={384}
+          platform="DESKTOP"
+          placeholder="내용을 입력해주세요"
+          value={accountInfo.message}
+          onChange={handleTitleMessageChange}
+        />
       </Column>
-      {accountOptions.map(({ label, key }) => (
-        <Column key={key} gap={8}>
-          <CheckBox
-            label={label}
-            checked={state[key]}
-            onChange={() => dispatch({ type: 'TOGGLE', payload: key })}
-          />
-          {state[key] && (
-            <Column gap={8}>
-              <Input width={384} platform="DESKTOP" placeholder="은행을 입력해주세요" />
-              <Input
-                width={384}
-                platform="DESKTOP"
-                placeholder="계좌번호를 입력해주세요"
-              />
-              <Input width={384} platform="DESKTOP" placeholder="예금주를 입력해주세요" />
-            </Column>
-          )}
-        </Column>
-      ))}
+      {accountOptions.map(({ label, key }) => {
+        const person = accountInfo[key];
+        console.log(accountInfo);
+        return (
+          <Column key={key} gap={8}>
+            <CheckBox
+              label={label}
+              checked={person.isVisible}
+              onChange={handleShowChange(key)}
+            />
+            {person.isVisible && (
+              <Column gap={8}>
+                <Input
+                  name="bank"
+                  width={384}
+                  platform="DESKTOP"
+                  placeholder="은행을 입력해주세요"
+                  value={person.bank}
+                  onChange={handlePersonInputChange(key)}
+                />
+                <Input
+                  name="accountNumber"
+                  width={384}
+                  platform="DESKTOP"
+                  placeholder="계좌번호를 입력해주세요"
+                  value={person.accountNumber}
+                  onChange={handlePersonInputChange(key)}
+                />
+                <Input
+                  name="accountHolder"
+                  width={384}
+                  platform="DESKTOP"
+                  placeholder="예금주를 입력해주세요"
+                  value={person.accountHolder}
+                  onChange={handlePersonInputChange(key)}
+                />
+              </Column>
+            )}
+          </Column>
+        );
+      })}
     </Column>
   );
 };
