60022fdfc1d799d8f5d9cca19571f70108d4f5a1
junghongseop <a01082372487@gmail.com>
2025-07-02 08:42:44 +0900

    fix/mobile(#105): 페이지에 이벤트 추가

--- apps/mobile/src/app/page.tsx
+++ apps/mobile/src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { ROUTES } from '@/constants/common/constant';
 import AppLayout from '@/layouts/AppLayout';
 import { getTemplateFontStyle } from '@merried/design-system';
 import { Column, LoginButton } from '@merried/ui';
@@ -10,19 +9,32 @@ import styled from 'styled-components';
 
 const Login = () => {
   const router = useRouter();
+  const kakao = process.env.NEXT_PUBLIC_KAKAO_LOGIN_URL;
+  const naver = process.env.NEXT_PUBLIC_NAVER_LOGIN_URL;
+
+  const handleNaverLogin = () => {
+    if (naver) {
+      router.push(naver);
+    } else {
+      alert('네이버 로그인 URL이 설정되어 있지 않습니다.');
+    }
+  };
+
+  const handleKakaoLogin = () => {
+    if (kakao) {
+      router.push(kakao);
+    } else {
+      alert('카카오 로그인 URL이 설정되어 있지 않습니다.');
+    }
+  };
 
   return (
     <AppLayout>
       <StyledLogin>
         <Text>우리 결혼할래요?</Text>
         <Column alignItems="center" width="100%" gap={12}>
-          <LoginButton
-            type="KAKAO"
-            onClick={() => {
-              router.push(ROUTES.HOME);
-            }}
-          />
-          <LoginButton type="NAVER" onClick={() => {}} />
+          <LoginButton type="KAKAO" onClick={handleKakaoLogin} />
+          <LoginButton type="NAVER" onClick={handleNaverLogin} />
         </Column>
       </StyledLogin>
     </AppLayout>
@@ -40,7 +52,7 @@ const StyledLogin = styled.div`
   width: 100%;
   height: 100vh;
   padding: 180px 12px 97px;
-  background-image: url('loginBackground.svg');
+  background-image: url('/loginBackground.svg');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
