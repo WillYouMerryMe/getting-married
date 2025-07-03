2d42df100c0236046bde8b3b5ea8969a4389e7d0
junghongseop <a01082372487@gmail.com>
2025-07-03 19:29:45 +0900

    fix/mobile(#118): Suspense로 프리렌더 문제 해결

--- apps/mobile/src/app/callback/naver/page.tsx
+++ apps/mobile/src/app/callback/naver/page.tsx
@@ -1,23 +1,14 @@
 'use client';
 
-import { useLoginMutation } from '@/services/auth/mutations';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useRef } from 'react';
+import { CallbackNaverContent } from '@/components/login';
+import { Suspense } from 'react';
 
 const CallbackNaver = () => {
-  const searchParams = useSearchParams();
-  const { loginMutate } = useLoginMutation();
-  const alreadyRun = useRef(false);
-
-  useEffect(() => {
-    const code = searchParams.get('code');
-    if (code && !alreadyRun.current) {
-      alreadyRun.current = true;
-      loginMutate({ code, provider: 'NAVER' });
-    }
-  }, [searchParams, loginMutate]);
-
-  return <></>;
+  return (
+    <Suspense>
+      <CallbackNaverContent />
+    </Suspense>
+  );
 };
 
 export default CallbackNaver;
