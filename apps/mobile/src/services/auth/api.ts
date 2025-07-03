71ad75082605654a183aa6cc61ae80eed7191ad7
junghongseop <a01082372487@gmail.com>
2025-07-03 20:42:24 +0900

    add/mobile(#113): 로그아웃 API 연동

--- apps/mobile/src/services/auth/api.ts
+++ apps/mobile/src/services/auth/api.ts
@@ -1,8 +1,15 @@
 import { married } from '@/apis/instance/instance';
+import authorization from '@/apis/token/token';
 import { PostLoginReq } from '@/types/auth/remote';
 
 export const postLogin = async ({ code, provider }: PostLoginReq) => {
   const { data } = await married.post('/auth/login', { code, provider });
+
+  return data;
+};
+
+export const postLogout = async (token: string) => {
+  const { data } = await married.post('/auth/logout', { token }, authorization());
 
   return data;
 };
