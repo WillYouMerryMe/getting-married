936a584c8e9789c7472e6fdbc09093ff9467891a
junghongseop <a01082372487@gmail.com>
2025-07-03 19:29:18 +0900

    feat/mobile(#118): CallbackKakao & CallbackNaver Content 컴포넌트 개발

---
+++ apps/mobile/src/components/login/CallbackKakaoContent/CallbackKakaoContent.tsx
@@ -0,0 +1,21 @@
+import { useLoginMutation } from '@/services/auth/mutations';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+
+const CallbackKakaoContent = () => {
+  const searchParams = useSearchParams();
+  const { loginMutate } = useLoginMutation();
+  const alreadyRun = useRef(false);
+
+  useEffect(() => {
+    const code = searchParams.get('code');
+    if (code && !alreadyRun.current) {
+      alreadyRun.current = true;
+      loginMutate({ code, provider: 'KAKAO' });
+    }
+  }, [searchParams, loginMutate]);
+
+  return <></>;
+};
+
+export default CallbackKakaoContent;
---
+++ apps/mobile/src/components/login/CallbackNaverContent/CallbackNaverContent.tsx
@@ -0,0 +1,21 @@
+import { useLoginMutation } from '@/services/auth/mutations';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+
+const CallbackNaverContent = () => {
+  const searchParams = useSearchParams();
+  const { loginMutate } = useLoginMutation();
+  const alreadyRun = useRef(false);
+
+  useEffect(() => {
+    const code = searchParams.get('code');
+    if (code && !alreadyRun.current) {
+      alreadyRun.current = true;
+      loginMutate({ code, provider: 'NAVER' });
+    }
+  }, [searchParams, loginMutate]);
+
+  return <></>;
+};
+
+export default CallbackNaverContent;
