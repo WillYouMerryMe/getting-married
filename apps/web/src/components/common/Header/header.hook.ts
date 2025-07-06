fa8f537640c7baded9d25d8540c1e90dfcb19daa
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-07-06 21:34:07 +0900

    feat/web(#122): 청접장 생성 기능 추가

---
+++ apps/web/src/components/common/Header/header.hook.ts
@@ -0,0 +1,128 @@
+import { PostCardReq } from '@/types/form/remote';
+import { useComponentOrderValueStore } from '@/store/form/orderState';
+import { useInvitationSetupValueStore } from '@/store/form/invitationSetup';
+import { useMainScreenValueStore } from '@/store/form/mainScreen';
+import { useInvitationMessageValueStore } from '@/store/form/invitationMessage';
+import { useCoupleIntroValueStore } from '@/store/form/coupleIntro';
+import { useCeremonyInfoValueStore } from '@/store/form/ceremonyInfo';
+import { useGalleryImageValueStore } from '@/store/form/galleryImage';
+import { useWeddingIntroValueStore } from '@/store/form/weddingIntro';
+import { useDirectionsValueStore } from '@/store/form/directions';
+import { useAccountInfoValueStore } from '@/store/form/account';
+import { useNoticeValueStore } from '@/store/form/notice';
+import { useGuestbookValueStore } from '@/store/form/guestbook';
+import { useGuestSnapValueStore } from '@/store/form/guestSnap';
+import { useUrlShareStyleValueStore } from '@/store/form/urlShareStyle';
+
+export const usePostCardParams = (): PostCardReq => {
+  const invitationSetup = useInvitationSetupValueStore();
+  const mainScreen = useMainScreenValueStore();
+  const invitationMessage = useInvitationMessageValueStore();
+  const coupleIntro = useCoupleIntroValueStore();
+  const groomProfile = coupleIntro.isToggle ? coupleIntro.groom : null;
+  const brideProfile = coupleIntro.isToggle ? coupleIntro.bride : null;
+  const ceremonyInfo = useCeremonyInfoValueStore();
+  const galleryImage = useGalleryImageValueStore();
+  const weddingIntro = useWeddingIntroValueStore();
+  const directions = useDirectionsValueStore();
+  const account = useAccountInfoValueStore();
+  const notice = useNoticeValueStore();
+  const guestbook = useGuestbookValueStore();
+  const guestSnap = useGuestSnapValueStore();
+  const urlShareStyle = useUrlShareStyleValueStore();
+  const componentOrder = useComponentOrderValueStore();
+
+  return {
+    title: '체크용',
+    templateId: '3',
+    invitationSetting: {
+      pointColor: invitationSetup.pointColor,
+      font: invitationSetup.invitationFont,
+    },
+    mainPageSetting: {
+      picture: mainScreen.image?.[0] ?? '',
+      font: mainScreen.letteringFont,
+      lettering: [mainScreen.letteringText],
+      letteringColor: mainScreen.letteringColor,
+    },
+    invitationMessage: invitationMessage.isToggle
+      ? {
+          title: invitationMessage.title,
+          content: invitationMessage.message,
+        }
+      : null,
+    groomProfile,
+    brideProfile,
+    weddingInfo: ceremonyInfo.isToggle
+      ? {
+          date: ceremonyInfo.calenderDate.toISOString(),
+          weddingHallName: ceremonyInfo.name,
+          isCalendarVisible: ceremonyInfo.isCalendarVisible,
+        }
+      : null,
+    photoGallery: galleryImage.isToggle
+      ? {
+          title: galleryImage.title,
+          urls: galleryImage.imageList || [],
+        }
+      : null,
+    videoGallery: weddingIntro.isToggle
+      ? {
+          title: weddingIntro.title,
+          url: weddingIntro.vedioURL,
+        }
+      : null,
+    locationGuide: directions.isToggle
+      ? {
+          address: directions.address,
+          isSubway: directions.show.subway,
+          subwayDetail: directions.methods.subway || '',
+          isBus: directions.show.bus,
+          busDetail: directions.methods.bus || '',
+          hasParking: directions.show.car,
+          parkingDetail: directions.methods.car || '',
+        }
+      : null,
+    accountInfo: account.isToggle
+      ? {
+          title: account.title,
+          content: account.message,
+          groom: account.groom,
+          bride: account.bride,
+          groomFather: account.groomFather,
+          groomMother: account.groomMother,
+          brideFather: account.brideFather,
+          brideMother: account.brideMother,
+        }
+      : null,
+    guestNotice: notice.isToggle
+      ? {
+          title: notice.title,
+          content: notice.message,
+        }
+      : null,
+    guestBook: guestbook.isToggle
+      ? {
+          title: guestbook.title,
+          masterPassword: guestbook.password,
+        }
+      : null,
+    guestSnapshots: guestSnap.isToggle
+      ? {
+          title: guestSnap.title,
+          masterPassword: guestSnap.password,
+        }
+      : null,
+    shareUrlStyle: urlShareStyle.isToggle
+      ? {
+          thumbnailUrl: '아무URL',
+          title: urlShareStyle.title,
+          content: urlShareStyle.message,
+        }
+      : null,
+    componentOrders: componentOrder.map((type, index) => ({
+      componentType: type,
+      order: index,
+    })),
+  };
+};
