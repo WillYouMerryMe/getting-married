ea14700893fda6421694005265527b67e39ecbbf
junghongseop <a01082372487@gmail.com>
2025-07-07 01:40:50 +0900

    aadd/mobile(#114): 청첩장 상세 조회 쿼리 작성

--- apps/mobile/src/services/card/queries.ts
+++ apps/mobile/src/services/card/queries.ts
@@ -1,11 +1,21 @@
 import { KEY } from '@/constants/common/constant';
 import { useQuery } from '@tanstack/react-query';
-import { getCardList } from './api';
+import { getCardDetail, getCardList } from './api';
 
 export const useCardListQuery = () => {
   const { data, ...restQuery } = useQuery({
     queryKey: [KEY.CARD_LIST],
     queryFn: getCardList,
+    retry: false,
+  });
+
+  return { data, ...restQuery };
+};
+
+export const useCardDetailQuery = (id: string) => {
+  const { data, ...restQuery } = useQuery({
+    queryKey: [KEY.CARD_DETAIL, id],
+    queryFn: () => getCardDetail(id),
     retry: false,
   });
 
