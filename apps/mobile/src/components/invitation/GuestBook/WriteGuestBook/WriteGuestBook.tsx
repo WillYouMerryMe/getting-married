6457e3cac725502fcc48a12776e8c0c881580045
junghongseop <a01082372487@gmail.com>
2025-07-09 11:49:03 +0900

    fix/mobile(#149): GuestBook 컴포넌트로 id 넘겨주도록 수정

--- apps/mobile/src/components/invitation/GuestBook/WriteGuestBook/WriteGuestBook.tsx
+++ apps/mobile/src/components/invitation/GuestBook/WriteGuestBook/WriteGuestBook.tsx
@@ -1,20 +1,53 @@
+import { useGuestBookCreate } from '@/services/guestbook/mutations';
+import { PostGuestBookCreateReq } from '@/types/guestbook/remote';
 import { Button, Column, Input, Text } from '@merried/ui';
 import { flex } from '@merried/utils';
+import { useState } from 'react';
 import styled from 'styled-components';
 
 interface WriteGuestBookProps {
   pointColor: string;
+  id: string;
 }
 
-const WriteGuestBook = ({ pointColor }: WriteGuestBookProps) => {
+const WriteGuestBook = ({ pointColor, id }: WriteGuestBookProps) => {
+  const [guestbook, setGuestbook] = useState<PostGuestBookCreateReq>({
+    cardId: id,
+    name: '',
+    content: '',
+  });
+  const { guestBookCreate } = useGuestBookCreate();
+
+  const handleGuestBookCreate = () => {
+    guestBookCreate(guestbook, {
+      onSuccess: () => {
+        setGuestbook({ cardId: id, name: '', content: '' });
+      },
+    });
+  };
+
   return (
     <StyledWriteGuestBook>
       <Column gap={12} width="100%">
-        <Input width="100%" label="이름" placeholder="이름을 입력해주세요" />
-        <Input width="100%" label="방명록 글귀" placeholder="글귀를 입력해주세요" />
+        <Input
+          width="100%"
+          label="이름"
+          placeholder="이름을 입력해주세요"
+          name="name"
+          value={guestbook.name}
+          onChange={(e) => setGuestbook((prev) => ({ ...prev, name: e.target.value }))}
+        />
+        <Input
+          width="100%"
+          label="방명록 글귀"
+          placeholder="글귀를 입력해주세요"
+          name="content"
+          value={guestbook.content}
+          onChange={(e) => setGuestbook((prev) => ({ ...prev, content: e.target.value }))}
+        />
       </Column>
       <Button
-        onClick={() => {}}
+        onClick={handleGuestBookCreate}
         width="100%"
         size="VERY_LARGE"
         pointColor={pointColor}
