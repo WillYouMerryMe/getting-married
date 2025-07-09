79842f5965668f61fa7f1ba13a6697deaf27b519
junghongseop <a01082372487@gmail.com>
2025-07-10 08:19:51 +0900

    fix/mobile(#155): 답례품 조건에 따른 인원 수 요청하도록 수정

--- apps/mobile/src/app/shop/send/send.tsx
+++ apps/mobile/src/app/shop/send/send.tsx
@@ -1,13 +1,34 @@
+import { ROUTES } from '@/constants/common/constant';
 import { GIFT, MEAL, PAID, STATUS } from '@/constants/shop/constants';
+import { useAttendee } from '@/services/attendee/queries';
 import { useAttendeeValueStore } from '@/stores/shop/attendee';
+import { useSetShopStepStore } from '@/stores/shop/shopStep';
 import { Gift, Meal, Paid, Status } from '@/types/shop/client';
 import { color } from '@merried/design-system';
 import { Button, Column, Text } from '@merried/ui';
 import { flex } from '@merried/utils';
+import { useRouter } from 'next/navigation';
 import { styled } from 'styled-components';
 
 const Send = () => {
   const attendee = useAttendeeValueStore();
+  const setShopStep = useSetShopStepStore();
+  const router = useRouter();
+
+  const attendeeStatus = attendee.status === 'ATTEND' ? true : false;
+  const attendeeGift = attendee.paid === 'PAID' ? true : false;
+  const attendeeMeal = attendee.meal === 'MEAL' ? true : false;
+
+  const { data } = useAttendee({
+    isAttendee: attendeeStatus,
+    hasSentGift: attendeeGift,
+    isEating: attendeeMeal,
+  });
+
+  const handleSendGift = () => {
+    router.push(ROUTES.HOME);
+    setShopStep('답례품목록');
+  };
 
   return (
     <StyledSend>
@@ -21,7 +42,7 @@ const Send = () => {
             <Text fontType="H3" color={color.G900}>
               조건을 만족한{' '}
               <Text fontType="H3" color={color.primary}>
-                34명
+                {data?.length}명
               </Text>
               에게
             </Text>
@@ -35,7 +56,7 @@ const Send = () => {
         </Column>
         <GiftCard />
       </Column>
-      <Button width="100%" size="VERY_LARGE" onClick={() => {}}>
+      <Button width="100%" size="VERY_LARGE" onClick={handleSendGift}>
         <Text fontType="Button3" color={color.G0}>
           답례품 보내기
         </Text>
