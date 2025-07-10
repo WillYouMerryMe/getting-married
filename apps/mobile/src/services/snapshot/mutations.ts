af7f0109d8a9fc61a31b0d357acc256986c36262
junghongseop <a01082372487@gmail.com>
2025-07-10 13:24:21 +0900

    fix/mobile(#154): React Query 작성

---
+++ apps/mobile/src/services/snapshot/mutations.ts
@@ -0,0 +1,18 @@
+import { useMutation } from '@tanstack/react-query';
+import { postGuestSanpShot } from './api';
+import { PostGuestSnapShotReq } from '@/types/snapshot/remote';
+
+export const useGusetSnapShotMutation = () => {
+  const { mutate: guestSnapShotMutate, ...restMutate } = useMutation({
+    mutationFn: ({ cardId, urls }: PostGuestSnapShotReq) =>
+      postGuestSanpShot({ cardId, urls }),
+    onSuccess: () => {
+      alert('사진 전송이 완료되었습니다.');
+    },
+    onError: () => {
+      alert('잠시후 다시 시도해주세요.');
+    },
+  });
+
+  return { guestSnapShotMutate, ...restMutate };
+};
